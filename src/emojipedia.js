760b452607eb7befc08c693bcce18c88846fbe1b
se-prashant <prashant791singh@gmail.com>
2021-05-28 17:47:48 +0530

    v1

---
+++ src/emojipedia.js
@@ -0,0 +1,38 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤗",
+    name: "Hugging Face",
+    meaning:
+      "Smiley with red cheeks is stretching out its hands toward you. Wants to hug you. It's an open and cordial gesture and an expression of warmth and friendliness."
+  },
+  {
+    id:5,
+    emoji :"😪",
+    name: "Sleepy Face",
+    meaning:
+    "Emoji is dead tired and would love to sleep now. The bubble emerging from the nose is a typical manga symbol for a sleeping character. Or as an expression that a conversation or excursion is extremely boring and drowsy."
+  },
+];
+export default emojipedia
